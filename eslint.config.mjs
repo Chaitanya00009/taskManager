67c1817bb73edee6fb48e2f2ff1a10a2329569e0
Chaitanya00009 <ncchaitanya07@gmail.com>
2026-06-06 19:00:11 +0530

    Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.js"],
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-useless-catch": "off",
+    },
+    languageOptions: { sourceType: "commonjs" },
+  },
+]);
